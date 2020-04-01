3c56286d6195bd7b9a98a5aebb8971df69471c62
jesusledesma15 <jesusproint998@gmail.com>
2020-04-01 17:57:37 +0200

    Añade constructor a la clase Bombo

---
+++ src/main/java/bingo/Bombo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Bombo {
+    
+    private ArrayList<Integer> numeros;
+
+    public Bombo() {
+        this.numeros = new ArrayList<>();
+    }
+    
+        
+}
