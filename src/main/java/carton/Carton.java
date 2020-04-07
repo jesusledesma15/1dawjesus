b1491942197042aa821471b85df05c9c3ddae339
jesusledesma15 <jesusproint998@gmail.com>
2020-04-07 12:35:40 +0200

    Añade constructor a la clase Carton

---
+++ src/main/java/carton/Carton.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carton;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Carton {
+    
+    private String [][] carton;
+    
+    
+}
