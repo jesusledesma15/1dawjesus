8b1f89320866e4a0ef6e2b2d0d968a45b79cab9c
jesusledesma15 <jesusproint998@gmail.com>
2020-04-09 19:22:23 +0200

    Añade cambios al método imprimirCartón()

--- src/main/java/carton/Carton.java
+++ src/main/java/carton/Carton.java
@@ -13,9 +13,12 @@ import java.util.Random;
  */
 public class Carton {
 
+    //Almacenará números, simbolos y espacios
     private String[][] carton;
 
+    //Inicializamos el array en el constructor por defecto
     public Carton() {
+        //El carton se creará verticalmente para que la lógica pueda seguir el orden natural de i j.
         carton = new String[9][3];
     }
 
@@ -27,65 +30,72 @@ public class Carton {
         this.carton = carton;
     }
 
+
+    //Este método permite crear el carton con todos los requisitos
     public void generarCarton() {
         int hasta = 10;
         int desde = 1;
         for (int i = 0; i < carton.length; i++) {
-            String n1 = getNumEntre(desde, hasta);
-            System.out.println("Columna " + i);
-            System.out.println("F1: " + n1);
-            String n2 = getNumEntre(desde, hasta);
-            System.out.println("F2: " + n2);
-            String n3 = getNumEntre(desde, hasta);
-            System.out.println("F3: " + n3 + "\n");
+            //Creacion de los números aleatorios por fila
+            String n1 = getNumEntre(desde, hasta), n2 = getNumEntre(desde, hasta), n3 = getNumEntre(desde, hasta);
+
             for (int j = 0; j < carton[0].length; j++) {
                 //Control para el primer numero de cada columna
+                //El número siempre va a ser mas chico por tres números que el numero "hasta" de esa columna
                 if (Integer.parseInt(n1) <= hasta - 3) {
                     carton[i][2] = n1;
                 } else {
+                    //Hasta que no salga un número con dicho requisito no podemos asignarlo a la primera posición
                     do {
                         n1 = getNumEntre(desde, hasta);
                     } while (Integer.parseInt(n1) > hasta - 3);
 
                 }
                 //Control para el segundo numero de cada columna
-                //El número siempre va a ser mas chico por dos números que el numero hasta de esa columna, además debe de ser mayor que el número de arriba
+                //El número siempre va a ser mas chico por dos números que el numero hasta de esa columna, además va a ser mayor que el número de arriba
                 if (Integer.parseInt(n2) <= hasta - 2 && Integer.parseInt(n2) > Integer.parseInt(n1)) {
                     carton[i][1] = n2;
                 } else {
+                    //Hasta que no salga un número con dichos requisitos no podemos asignarlo a la primera posición
                     do {
                         n2 = getNumEntre(desde, hasta);
                     } while (Integer.parseInt(n2) > hasta - 2 || Integer.parseInt(n2) <= Integer.parseInt(n1));
                 }
 
                 //Control para el tercer numero de cada columna
+                //El número siempre va a ser mayor que el número de arriba
                 if (Integer.parseInt(n3) > Integer.parseInt(n2)) {
                     carton[i][0] = n3;
                 } else {
+                    //Hasta que no salga un número con dichos requisitos no podemos asignarlo a la primera posición
                     do {
                         n3 = getNumEntre(desde, hasta);
                     } while (Integer.parseInt(n3) <= Integer.parseInt(n2));
                 }
             }
+            //Actualizamos los números que tendrá la siguiente fila
             desde += 10;
             hasta += 10;
         }
+
     }
 
+    //Devuelve un String con un numero aleatorio entre desde y hasta 
     private String getNumEntre(int desde, int hasta) {
         Random rdn = new Random();
         return String.valueOf(rdn.nextInt(hasta - desde + 1) + desde);
     }
 
     public void imprimirCarton() {
-        for (int i = 0; i < carton.length; i++) {
-            for (int j = 0; j < carton[0].length; j++) {
-                System.out.print(i + "" + j + " " + carton[i][j] + "\t");
-                if (j == 2) {
+        //Al tener la lógica montada para un cartón vertical, tenemos que imprimirlo en horizontal, por lo tanto invertimos i y j.
+        //También tenemos que invertir j ya que el menor irá arriba y el menor irá abajo
+        for (int j = carton[0].length - 1; j >= 0; j--) {
+            for (int i = 0; i < carton.length; i++) {
+                System.out.print("j:" + j + " i:" + i + " " + carton[i][j] + "\t");
+                if (i == 8) { //Cuando i vale 8 es cuando se produce el salto a una nueva fila del carton
                     System.out.println("");
                 }
             }
-
         }
     }
 
