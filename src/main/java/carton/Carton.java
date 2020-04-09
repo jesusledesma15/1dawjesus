7add66a463b98fd8a5400ef8dc2562cf8b13a63a
jesusledesma15 <jesusproint998@gmail.com>
2020-04-09 13:43:41 +0200

    Añade control del primer numero de cada columna en el metodo generarCarton()

--- src/main/java/carton/Carton.java
+++ src/main/java/carton/Carton.java
@@ -28,10 +28,29 @@ public class Carton {
     }
 
     public void generarCarton() {
+        int hasta = 10;
+        int desde = 1;
         for (int i = 0; i < carton.length; i++) {
+            String n1 = getNumEntre(desde, hasta);
+            System.out.println("F1: " + n1);
+            String n2 = getNumEntre(desde, hasta);
+            System.out.println("F2: " + n2);
+            String n3 = getNumEntre(desde, hasta);
+            System.out.println("F3: " + n3 + "\n");
             for (int j = 0; j < carton[0].length; j++) {
-                carton[i][j] = getNumEntre(1, 90);
+                //Control para el primer numero de la columna x del carton
+                if (Integer.parseInt(n1) <= hasta - 3) {
+                    carton[i][2] = n1;
+                } else {
+                    do {
+                        n1 = getNumEntre(desde, hasta);
+                    } while (Integer.parseInt(n1) > hasta - 3);
+
+                }
+                
             }
+            desde += 10;
+            hasta += 10;
         }
     }
 
@@ -43,8 +62,8 @@ public class Carton {
     public void imprimirCarton() {
         for (int i = 0; i < carton.length; i++) {
             for (int j = 0; j < carton[0].length; j++) {
-                System.out.print(carton[i][j] + "\t");
-                if (j == 2 && (i == 2 || i == 5)) {
+                System.out.print(i + "" + j + " " + carton[i][j] + "\t");
+                if (j == 2) {
                     System.out.println("");
                 }
             }
