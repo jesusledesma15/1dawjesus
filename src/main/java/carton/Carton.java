6ec5308e542b276052e0fe03c20bb005e306e3e5
jesusledesma15 <jesusproint998@gmail.com>
2020-04-11 12:23:08 +0200

    Añade corrección de intervalos de números por columna

--- src/main/java/carton/Carton.java
+++ src/main/java/carton/Carton.java
@@ -33,33 +33,41 @@ public class Carton {
 
     //Este método permite crear el carton con todos los requisitos
     public void generarCarton() {
-        int hasta = 10;
+        int hasta = 9;
         int desde = 1;
         for (int i = 0; i < carton.length; i++) {
+            //Este if controla que apartir de la segunda columna en el carton puedan empezar por 10,20,30,40,50,60,70,80
+            if (i == 1) {
+                desde--;
+            }
+            //Este if permite que la ultima columna pueda tener el 90 también.
+            if (i == 8) {
+                hasta++;
+            }
             //Creacion de los números aleatorios por fila
             String n1 = getNumEntre(desde, hasta), n2 = getNumEntre(desde, hasta), n3 = getNumEntre(desde, hasta);
 
             for (int j = 0; j < carton[0].length; j++) {
                 //Control para el primer numero de cada columna
                 //El número siempre va a ser mas chico por tres números que el numero "hasta" de esa columna
-                if (Integer.parseInt(n1) <= hasta - 3) {
+                if (Integer.parseInt(n1) <= hasta - 4) {
                     carton[i][2] = n1;
                 } else {
                     //Hasta que no salga un número con dicho requisito no podemos asignarlo a la primera posición
                     do {
                         n1 = getNumEntre(desde, hasta);
-                    } while (Integer.parseInt(n1) > hasta - 3);
+                    } while (Integer.parseInt(n1) > hasta - 4);
 
                 }
                 //Control para el segundo numero de cada columna
                 //El número siempre va a ser mas chico por dos números que el numero hasta de esa columna, además va a ser mayor que el número de arriba
-                if (Integer.parseInt(n2) <= hasta - 2 && Integer.parseInt(n2) > Integer.parseInt(n1)) {
+                if (Integer.parseInt(n2) <= hasta - 3 && Integer.parseInt(n2) > Integer.parseInt(n1)) {
                     carton[i][1] = n2;
                 } else {
                     //Hasta que no salga un número con dichos requisitos no podemos asignarlo a la primera posición
                     do {
                         n2 = getNumEntre(desde, hasta);
-                    } while (Integer.parseInt(n2) > hasta - 2 || Integer.parseInt(n2) <= Integer.parseInt(n1));
+                    } while (Integer.parseInt(n2) > hasta - 3 || Integer.parseInt(n2) <= Integer.parseInt(n1));
                 }
 
                 //Control para el tercer numero de cada columna
@@ -91,7 +99,7 @@ public class Carton {
         //También tenemos que invertir j ya que el menor irá arriba y el menor irá abajo
         for (int j = carton[0].length - 1; j >= 0; j--) {
             for (int i = 0; i < carton.length; i++) {
-                System.out.print("j:" + j + " i:" + i + " " + carton[i][j] + "\t");
+                System.out.print("i:" + i + " j:" + j + " " + carton[i][j] + "\t");
                 if (i == 8) { //Cuando i vale 8 es cuando se produce el salto a una nueva fila del carton
                     System.out.println("");
                 }
