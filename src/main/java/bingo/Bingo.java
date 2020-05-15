5c3a04d641c26a16a2be6c962cd3622d4b786d8c
jesusledesma15 <jesusproint998@gmail.com>
2020-05-15 17:33:31 +0200

    Creación clase abstract Bingo

---
+++ src/main/java/bingo/Bingo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Jesus
+ */
+public abstract class Bingo {
+    private String id;
+    private LocalDate fecha;
+    private String idJugador;
+    
+}
