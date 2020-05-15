2184598765cd857eb0b2dbf443282a5a3bffd588
jesusledesma15 <jesusproint998@gmail.com>
2020-05-15 18:49:54 +0200

    Cambia clase Carton a paquete bingo

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package carton;
+package bingo;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
