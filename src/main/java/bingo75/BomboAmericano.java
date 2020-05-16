9c7584e0a3afe4aaa4462f1c2acff174220cc197
jesusledesma15 <jesusproint998@gmail.com>
2020-05-16 17:27:29 +0200

    Creacion de la clase BomboAmericano

---
+++ src/main/java/bingo75/BomboAmericano.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo75;
+
+import bingo.Bombo;
+
+/**
+ *
+ * @author Jesus
+ */
+public final class BomboAmericano extends Bombo {
+
+
+}
