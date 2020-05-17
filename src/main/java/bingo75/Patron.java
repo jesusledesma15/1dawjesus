4be60633e6be8a188533875f137fe98d86756c71
jesusledesma15 <jesusproint998@gmail.com>
2020-05-17 11:30:21 +0200

    Creación enum Patron

---
+++ src/main/java/bingo75/Patron.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bingo75;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jesus
+ */
+public enum Patron {
+    
+    CARTON_LLENO(),
+    FORMA_X(),
+    FORMA_Y;
+    
+    private ArrayList<Point> casillas;
+    private String descripcion;
+    
+    
+}
