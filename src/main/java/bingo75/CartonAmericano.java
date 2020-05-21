5e21801b15cbee11e97c99b52b50793a5b67dbdd
jesusledesma15 <jesusproint998@gmail.com>
2020-05-21 19:26:03 +0200

    Añade metodo generarCarton() CartonAmericano

--- src/main/java/bingo75/CartonAmericano.java
+++ src/main/java/bingo75/CartonAmericano.java
@@ -6,6 +6,7 @@
 package bingo75;
 
 import bingo.Carton;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -40,24 +41,45 @@ public final class CartonAmericano extends Carton {
         }
     }
 
-//    private ArrayList<Integer> numDistintos(int desde, int hasta) {
-//        ArrayList<Integer> numDistintos = new ArrayList<>();
-//        int numero;
-//        while (numDistintos.size() != 5) {
-//            numero = getNumEntre(desde, hasta);
-//            if (!numDistintos.contains(numero)) {
-//                numDistintos.add(numero);
-//            }
-//        }
-//
-//        return numDistintos;
-//    }
+    private ArrayList<Integer> numDistintos(int desde, int hasta) {
+        ArrayList<Integer> numDistintos = new ArrayList<>();
+        int numero;
+        while (numDistintos.size() != 6) {
+            numero = getNumEntre(desde, hasta);
+            if (!numDistintos.contains(numero)) {
+                numDistintos.add(numero);
+            }
+        }
+
+        return numDistintos;
+    }
+
     @Override
     public void generarCarton() {
+        ArrayList<Integer> y0 = numDistintos(1, 15);
+        ArrayList<Integer> y1 = numDistintos(16, 30);
+        ArrayList<Integer> y2 = numDistintos(31, 45);
+        ArrayList<Integer> y3 = numDistintos(46, 60);
+        ArrayList<Integer> y4 = numDistintos(61, 75);
         for (int i = 0; i < premio.getCasillas().size(); i++) {
-            for (int j = 0; j < premio.getCasillas().size(); j++) {
-                getCarton()[0][0] = 1;
+            switch (premio.getCasillas().get(i).y) {
+                case 0:
+                    getCarton()[premio.getCasillas().get(i).x][premio.getCasillas().get(i).y] = y0.remove(y0.size() - 1);
+                    break;
+                case 1:
+                    getCarton()[premio.getCasillas().get(i).x][premio.getCasillas().get(i).y] = y1.remove(y1.size() - 1);
+                    break;
+                case 2:
+                    getCarton()[premio.getCasillas().get(i).x][premio.getCasillas().get(i).y] = y2.remove(y2.size() - 1);
+                    break;
+                case 3:
+                    getCarton()[premio.getCasillas().get(i).x][premio.getCasillas().get(i).y] = y3.remove(y3.size() - 1);
+                    break;
+                case 4:
+                    getCarton()[premio.getCasillas().get(i).x][premio.getCasillas().get(i).y] = y4.remove(y4.size() - 1);
+                    break;
             }
+
         }
     }
 
