7a6d4fe0897131287a22833f2e376a6e5bb858ca
jesusledesma15 <jesusproint998@gmail.com>
2020-06-06 18:56:32 +0200

    Añade metodo getAll() en BingoDAO

--- src/main/java/bingo75/BingoDAO.java
+++ src/main/java/bingo75/BingoDAO.java
@@ -10,7 +10,10 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -38,7 +41,7 @@ public class BingoDAO {
             prest.setString(3, partida.getIdjugador());
             prest.setInt(4, partida.getTipo());
             prest.setString(5, partida.bomboToString());
-            prest.setString(6, Arrays.deepToString(partida.getCarton()));
+            prest.setString(6, partida.cartonToString());
 
             numFilas = prest.executeUpdate() > 0;
         } catch (SQLException e) {
@@ -48,4 +51,45 @@ public class BingoDAO {
         return numFilas;
     }
 
+    public List<BingoVO> getAll() throws SQLException {
+        List<BingoVO> lista = new ArrayList<>();
+
+        // Preparamos la consulta de datos mediante un objeto Statement
+        // ya que no necesitamos parametrizar la sentencia SQL
+        try (Statement st = con.createStatement()) {
+            // Ejecutamos la sentencia y obtenemos las filas en el objeto ResultSet
+            ResultSet res = st.executeQuery("select * from partidas");
+            // Ahora construimos la lista, recorriendo el ResultSet y mapeando los datos
+            while (res.next()) {
+                BingoVO b = new BingoVO();
+                // Recogemos los datos de la partida, guardamos en un objeto
+
+                b.setId(res.getString("id"));
+                b.setFecha(res.getDate("fecha").toLocalDate());
+                b.setIdjugador(res.getString("idjugador"));
+                b.setTipo(res.getInt("tipo"));
+                b.setBombo(BingoVO.stringBomboToArrayList(res.getString("bombo")));
+                b.setCarton(BingoVO.stringCartonToArrayInt(res.getString("carton"), b.getTipo()));
+
+                System.out.println("id: " + b.getId());
+                System.out.println("fecha: " + b.getFecha());
+                System.out.println("idjugador: " + b.getIdjugador());
+                System.out.println("tipo: " + b.getTipo());
+                System.out.println("Bombo: " + b.getBombo());
+                System.out.println("Carton: " + Arrays.deepToString(b.getCarton()));
+
+                //Añadimos el objeto a la lista
+                lista.add(b);
+            }
+        }
+
+        return lista;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        BingoDAO bdDao = new BingoDAO();
+        List<BingoVO> lista = bdDao.getAll();
+        lista.forEach(System.out::println);
+
+    }
 }
