56ce8af7193945dbab0b840f21dc6a50321d16d6
jesusledesma15 <jesusproint998@gmail.com>
2020-06-06 21:22:21 +0200

    Añade metodo getId() en BingoDAO

--- src/main/java/bingo75/BingoDAO.java
+++ src/main/java/bingo75/BingoDAO.java
@@ -86,6 +86,18 @@ public class BingoDAO {
         return lista;
     }
 
+    public static String getId() throws SQLException {
+        BingoDAO bdDao = new BingoDAO();
+        int id;
+        List<BingoVO> lista = bdDao.getAll();
+        if (!lista.isEmpty()) {
+            id = Integer.parseInt(lista.get(lista.size()-1).getId())+1;
+            return String.valueOf(id);
+        }
+        
+        return "0";
+    }
+
     public static void main(String[] args) throws SQLException {
         BingoDAO bdDao = new BingoDAO();
         List<BingoVO> lista = bdDao.getAll();
